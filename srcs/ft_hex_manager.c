e6ab1dcc3683e50851436e7005b66e2d0464bea1
brian <bvalette@student.42.fr>
2020-01-07 09:31:39 +0100

    clean hex_conv

--- srcs/ft_hex_manager.c
+++ srcs/ft_hex_manager.c
@@ -6,41 +6,12 @@
 /*   By: bvalette <bvalette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/28 13:46:56 by bvalette          #+#    #+#             */
-/*   Updated: 2020/01/07 09:30:52 by bvalette         ###   ########.fr       */
+/*   Updated: 2020/01/07 09:31:25 by bvalette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdlib.h>
-
-/*
-static char	*ft_zero_padding(t_format *format, char *hex_str, int pre)
-{
-	char		*padded_ret;
-	size_t		hex_len;
-	size_t		padded_len;
-	int			offset;
-
-	offset = 0;
-	if (format->conv == 'p' || ft_str_set(format->flag, "#") != 0)
-		offset = 2;
-	hex_len = ft_strlen(hex_str);
-	if (pre < 0)
-		pre = 0;
-	padded_len = hex_len + offset;
-	if (pre > (int)hex_len)
-		padded_len = pre + offset;
-	padded_ret = (char *)ft_calloc(padded_len + 1, sizeof(char));
-	if (padded_ret == NULL)
-		return (NULL);
-	ft_memset(padded_ret, '0', padded_len);
-	if (format->conv == 'p' || ft_str_set(format->flag, "#") != 0)
-		ft_memcpy(padded_ret, "0x", 2);
-	ft_memcpy(padded_ret + (padded_len - hex_len), hex_str, hex_len);
-	free(hex_str);
-	return (padded_ret);
-}
-*/
 
 static char	*ft_zero_padding(t_format *format, char *hex_str)
 {
